661dfeebf102d013e405cd1303c45ee9c789c7d2
antonisierakowski <antonisierakowski@gmail.com>
2020-07-30 20:12:39 +0200

    Split PdfPresentation and PdfPage components to prevent multiple requests

--- src/components/Presentation/PdfPresentation/index.tsx
+++ src/components/Presentation/PdfPresentation/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Document, Page } from 'react-pdf/dist/entry.webpack';
 import { usePresentation } from '../hook';
 import styles from './styles.module.css';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../store/types';
+import { selectCurrentPresentationUrl } from '../../../store/presentation/selectors';
 
 export const PdfPresentation: React.FC = (): React.ReactElement => {
-  const { currentSlide, presentationUrl } = usePresentation();
-
+  const presentationUrl = useSelector((state: RootState) =>
+    selectCurrentPresentationUrl(state),
+  );
   return (
     <Document file={presentationUrl} className={styles.presentationWrapper}>
-      <Page pageNumber={currentSlide} renderAnnotationLayer={false} />
+      <PdfPage />
     </Document>
   );
+};
+
+const PdfPage: React.FC = (): React.ReactElement => {
+  const { currentSlide } = usePresentation();
+
+  return <Page pageNumber={currentSlide} renderAnnotationLayer={false} />;
 };
