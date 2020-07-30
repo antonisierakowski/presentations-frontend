485045b1837bc7315705480d5c2f9fafa5f9af5d
antonisierakowski <antonisierakowski@gmail.com>
2020-07-30 18:19:47 +0200

    General layout styling

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { NotificationContainer } from '../Notification/NotificationContainer';
-import styles from './styles.module.css';
 import { Routing } from '../Routing';
 
 export const App: React.FC = () => (
-  <div className={styles.App}>
+  <>
     <NotificationContainer />
     <Routing />
-  </div>
+  </>
 );
