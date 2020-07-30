0f8b399c0b5f26f0fdf3544bed86ca49326b882b
antonisierakowski <antonisierakowski@gmail.com>
2020-07-30 19:05:36 +0200

    Style notification icons

--- src/components/Notification/Notification/index.tsx
+++ src/components/Notification/Notification/index.tsx
@@ -1,6 +1,6 @@
 import { Error, Done, Info, Close } from '@material-ui/icons';
 import React from 'react';
-import { Card, Typography } from '@material-ui/core';
+import { Card, SvgIconTypeMap, Typography } from '@material-ui/core';
 import styles from './styles.module.css';
 import { NotificationType } from '../../../store/notifications/model';
 import { useNotification } from './hook';
@@ -16,7 +16,9 @@ export const Notification: React.FC<Props> = ({ id }) => {
     <Card className={styles.notification} elevation={3} data-testid={id}>
       <div className={styles.notificationBody}>
         {getIconType(notification.notificationType)}
-        <Typography variant="subtitle2">{notification.textContent}</Typography>
+        <Typography variant="subtitle2" className={styles.notificationText}>
+          {notification.textContent}
+        </Typography>
       </div>
       <Close className={styles.closeIcon} onClick={onClose} />
     </Card>
@@ -26,13 +28,17 @@ export const Notification: React.FC<Props> = ({ id }) => {
 const getIconType = (notificationType: NotificationType) => {
   switch (notificationType) {
     case NotificationType.SUCCESS: {
-      return <Done />;
+      return <Done {...iconProps} />;
     }
     case NotificationType.FAILURE: {
-      return <Error />;
+      return <Error {...iconProps} />;
     }
     default: {
-      return <Info />;
+      return <Info {...iconProps} />;
     }
   }
+};
+
+const iconProps: SvgIconTypeMap['props'] = {
+  color: 'secondary',
 };
