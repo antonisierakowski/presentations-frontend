7fa4dfec69cbde978be6e51185e3d1a57c02e02a
antonisierakowski <antonisierakowski@gmail.com>
2020-07-30 19:14:14 +0200

    Split UploadingIndicator as separate component from IndexPage

--- src/components/Pages/IndexPage/index.tsx
+++ src/components/Pages/IndexPage/index.tsx
@@ -1,46 +1,24 @@
 import React from 'react';
 import { PresentationInput } from '../../PresentationInput';
 import styles from './styles.module.css';
-import { useSelector } from 'react-redux';
-import {
-  selectIsFileUploading,
-  selectUploadingFileName,
-} from '../../../store/app/selectors';
-import { RootState } from '../../../store/types';
 import { Paper, Typography } from '@material-ui/core';
-import { CircularProgress } from '@material-ui/core';
+import { UploadingIndicator } from '../../UploadingIndicator';
 
-export const IndexPage: React.FC = () => {
-  const isUploading = useSelector((state: RootState) =>
-    selectIsFileUploading(state),
-  );
-  const uploadingFileName = useSelector((state: RootState) =>
-    selectUploadingFileName(state),
-  );
-
-  return (
-    <div className={styles.presentationInputPage}>
-      <Paper className={styles.mainWrapper} elevation={5}>
-        <div className={styles.heading}>
-          <Typography variant="subtitle2">
-            Present it. Recruitment task for Lama Media.
-          </Typography>
-          <Typography variant="body2">
-            To start, upload a .pdf or .pptx file. Once your file is uploaded,
-            you can share the generated link with your viewers. Only you will be
-            able to change currently viewed slide or end the presentation.
-          </Typography>
-        </div>
-        <PresentationInput />
-        {isUploading && (
-          <div className={styles.uploading}>
-            <CircularProgress className={styles.spinner} size={20} />
-            <Typography variant="subtitle2">
-              Uploading {uploadingFileName}...
-            </Typography>
-          </div>
-        )}
-      </Paper>
-    </div>
-  );
-};
+export const IndexPage: React.FC = () => (
+  <div className={styles.presentationInputPage}>
+    <Paper className={styles.mainWrapper} elevation={5}>
+      <div className={styles.heading}>
+        <Typography variant="subtitle2">
+          Present it. Recruitment task for Lama Media.
+        </Typography>
+        <Typography variant="body2">
+          To start, upload a .pdf or .pptx file. Once your file is uploaded, you
+          can share the generated link with your viewers. Only you will be able
+          to change currently viewed slide or end the presentation.
+        </Typography>
+      </div>
+      <PresentationInput />
+      <UploadingIndicator />
+    </Paper>
+  </div>
+);
---
+++ src/components/UploadingIndicator/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store/types';
+import {
+  selectIsFileUploading,
+  selectUploadingFileName,
+} from '../../store/app/selectors';
+import styles from './styles.module.css';
+import { CircularProgress, Paper, Typography } from '@material-ui/core';
+
+export const UploadingIndicator: React.FC = () => {
+  const isUploading = useSelector((state: RootState) =>
+    selectIsFileUploading(state),
+  );
+  const uploadingFileName = useSelector((state: RootState) =>
+    selectUploadingFileName(state),
+  );
+
+  if (isUploading) {
+    return (
+      <div className={styles.uploading}>
+        <CircularProgress className={styles.spinner} size={20} />
+        <Typography variant="subtitle2">
+          Uploading {uploadingFileName}...
+        </Typography>
+      </div>
+    );
+  }
+
+  return null;
+};
